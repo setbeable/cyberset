610f163ba844dc165eca9c4cfbd271b8ed23ccf6
setbeable <65809334+setbeable@users.noreply.github.com>
2025-06-07 22:47:15 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 	import { loadNews } from './news.js';
 	import { loadLinks } from './data.js';
-	import { checkLogin, logout, toggleDarkMode, handleSearch, initSession } from './script.js';
+	import {
+	  checkLogin,
+	  logout,
+	  toggleDarkMode,
+	  handleSearch,
+	  initSession,
+	} from './script.js';
 
 	window.checkLogin = checkLogin;
 	window.logout = logout;
 
+	document.getElementById("loginBtn").addEventListener("click", checkLogin);
+	document.getElementById("logoutButton").addEventListener("click", logout);
 	document.getElementById("toggleDark").addEventListener("click", toggleDarkMode);
 	document.getElementById("search").addEventListener("input", handleSearch);
 
