49b92b6e5942e06cc7e803b6fe3b595d85cf83fa
setbeable <65809334+setbeable@users.noreply.github.com>
2025-06-07 22:38:11 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+	import { loadNews } from './news.js';
+	import { loadLinks } from './data.js';
+	import { checkLogin, logout, toggleDarkMode, handleSearch, initSession } from './script.js';
+
+	window.checkLogin = checkLogin;
+	window.logout = logout;
+
+	document.getElementById("toggleDark").addEventListener("click", toggleDarkMode);
+	document.getElementById("search").addEventListener("input", handleSearch);
+
+	window.addEventListener("DOMContentLoaded", () => {
+	  initSession(loadLinks, loadNews);
+	  setInterval(loadNews, 600000); // ogni 10 minuti
+	});
